036183f5b9a1aac776d83b7737949241f939c4af
Ed Tewiah <etewiah@hotmail.cim>
2013-02-26 21:48:00 +0100

    Initial commit

---
+++ app/models/event.rb
@@ -0,0 +1,3 @@
+class Event < ActiveRecord::Base
+  attr_accessible :body, :title
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TraccarRails::Application.config.secret_token = '8d7b4e2140629c1e65290583c2c845ce7b6e360c578f6e54d9cec1e8e24216f5d4076aa64b97d0266d55acc6b5a30256a9344e8b8ebd706820dfa0310d38d789'
